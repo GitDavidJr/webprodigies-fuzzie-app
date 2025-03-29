846b9fb31b051d74bd2b14db937f2f451d55f8f7
David Júnior <146877886+GitDavidJr@users.noreply.github.com>
2025-03-29 16:02:28 -0300

    feat: Implement EditorCanvasCardSingle component and add helper icon
    This commit implements the `EditorCanvasCardSingle` component, which is a single card within the editor canvas. It also adds a helper icon to the card.
    
    Changes include:
    
    -   Added `EditorCanvasCardSingle` component to `src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-card-single.tsx`.
    -   Imported `EditorCanvasCardType` from `src/lib/types.ts`.
    -   Imported `useEditor` from `src/providers/editor-provider`.
    -   Imported `useNodeId` from `@xyflow/react`.
    -   Added `EditorCanvasCardHelperIcon` component.
    -   Added conditional rendering for the `CustomGadle` component based on the card type.

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-card-single.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-card-single.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { EditorCanvasCardType } from "@/src/lib/types";
+import { useEditor } from "@/src/providers/editor-provider";
+import { useNodeId } from "@xyflow/react";
+import React, { useMemo } from "react";
+import EditorCanvasCardHelperIcon from "./editor-canvas-card-helper-icon";
 
-type Props = {}
+type Props = {};
 
-const EditorCanvasCardSingle = (props: Props) => {
+const EditorCanvasCardSingle = ({ data }: { data: EditorCanvasCardType }) => {
+  const { dispatch, state } = useEditor();
+  const nodeId = useNodeId();
+  const logo = useMemo(() => {
+    return <EditorCanvasCardHelperIcon type={data.type}/>;
+  }, [data]);
 
-    return (<div>EditorCanvasCardSingle</div>)
-}
+  return 
+    <>
+      {data.type == 'Trigger' && data.type !== "Google Drive" && (
+        <CustomGadle
+          type={'target'}
+          position={Position.Top}
+          styles={{ zIndex:100}}          
+        />
+      )}
+    </>;
+};
 
-export default EditorCanvasCardSingle
+export default EditorCanvasCardSingle;
